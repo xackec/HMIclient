672db4d3b559b95219d70f5a1515c786efa29b55
kirill <kirill@MacBook-Pro-kirill.local>
2018-07-23 21:47:43 +0300

    Initial commit from local git

---
+++ HMIclient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HMIclient
+//
+//  Created by Кирилл on 23.07.2018.
+//  Copyright © 2018 dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ HMIclientTests/HMIclientTests.swift
@@ -0,0 +1,34 @@
+//
+//  HMIclientTests.swift
+//  HMIclientTests
+//
+//  Created by Кирилл on 23.07.2018.
+//  Copyright © 2018 dev. All rights reserved.
+//
+
+import XCTest
+@testable import HMIclient
+
+class HMIclientTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
