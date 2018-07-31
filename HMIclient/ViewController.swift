c19ed6fe00f37e5d4f621093b2572864735b937b
kirill <kirill@MacBook-Pro-kirill.local>
2018-07-31 22:09:50 +0300

    commit from Xcode

--- HMIclient/ViewController.swift
+++ HMIclient/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var getRest: UIButton!
+    var text:String?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func getRestClick(){
+        guard let rest = RestController.make(urlString: "http://192.168.0.101:8080/greeting") else {
+            print("Bad URL")
+            return
+        }
+        
+        rest.get(withDeserializer: JSONDeserializer()) { result, httpResponse in
+            do {
+                let json = try result.value()
+               // print(json["content"].string) // "http://httpbin.org/get
+                self.text = json["content"].string
+            } catch {
+                print("Error performing GET: \(error)")
+            }
+        }
+        textField.text = text
+    }
 
 }
 
